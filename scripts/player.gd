dde545bb653af3d936874401a661cbf9da06fcca
kemini0615 <moeday1010@gmail.com>
2026-06-28 13:59:19 +0900

    플레이어 이동 애니메이션 추가

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,3 +11,14 @@ func _physics_process(_delta: float) -> void:
 	# velocity를 설정해도 move_and_slide()를 호출하지 않으면, 오브젝트는 움직이지 않는다
 	velocity = direction * speed
 	move_and_slide()
+	play_animation()
+
+func play_animation():
+	if direction:
+		$AnimatedSprite2D.flip_h = direction.x > 0 # 오른쪽으로 이동하면 X축 플립
+		if direction.x != 0:
+			$AnimatedSprite2D.animation = 'left'
+		else:
+			$AnimatedSprite2D.animation = 'down' if direction.y > 0 else 'up'	
+	else:
+		$AnimatedSprite2D.frame = 0 # 애니메이션 프레임을 수동으로 고정
