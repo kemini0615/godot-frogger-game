21cbec09db995e4ad5873dfdf9ae4defababc34d
kemini0615 <moeday1010@gmail.com>
2026-06-30 22:37:16 +0900

    레벨 생성

--- scripts/car.gd
+++ scripts/car.gd
@@ -5,6 +5,12 @@ class_name Car
 var direction: Vector2 = Vector2.ZERO
 var speed: float = 100
 
+var variations = [
+	preload("res://graphics/cars/green.png"),
+	preload("res://graphics/cars/red.png"),
+	preload("res://graphics/cars/yellow.png")
+]
+
 
 func _ready() -> void:
 	if position.x > 0:
@@ -12,6 +18,8 @@ func _ready() -> void:
 	else:
 		direction = Vector2.RIGHT
 		$Sprite2D.flip_h = true
+		
+	$Sprite2D.texture = variations.pick_random()
 
 
 func _process(delta: float) -> void:
